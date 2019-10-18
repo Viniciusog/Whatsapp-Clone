9bc6f4b47b92f380f5bb92dff478e3573a42fd8c
Viniciusog <vinijipa23@gmail.com>
2019-10-18 11:01:59 -0400

    add chat activity

--- app/src/main/java/com/viniciusog/whatsapp/fragment/ContatosFragment.java
+++ app/src/main/java/com/viniciusog/whatsapp/fragment/ContatosFragment.java
@@ -1,6 +1,7 @@
 package com.viniciusog.whatsapp.fragment;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,12 +11,17 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
+import com.viniciusog.whatsapp.Helper.RecyclerItemClickListener;
+import com.viniciusog.whatsapp.Helper.UsuarioFirebase;
 import com.viniciusog.whatsapp.R;
+import com.viniciusog.whatsapp.activity.ChatActivity;
 import com.viniciusog.whatsapp.adapter.ContatosAdapter;
 import com.viniciusog.whatsapp.config.ConfiguracaoFirebase;
 import com.viniciusog.whatsapp.model.Usuario;
@@ -36,6 +42,7 @@ public class ContatosFragment extends Fragment {
     private List<Usuario> listaContatos = new ArrayList<>();
     private DatabaseReference usuariosRef;
     private ValueEventListener valueEventListenerContatos;
+    FirebaseUser usuarioAtual;
 
     public ContatosFragment() {
         // Required empty public constructor
@@ -52,6 +59,7 @@ public class ContatosFragment extends Fragment {
         recyclerViewListaContatos = view.findViewById(R.id.recyclerViewListaContatos);
         //pegando referência dos usuários
         usuariosRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuarios");
+        usuarioAtual = UsuarioFirebase.getUsuarioAtual();
 
         //Configura adapter
         adapter = new ContatosAdapter( listaContatos, getActivity());
@@ -61,6 +69,31 @@ public class ContatosFragment extends Fragment {
         recyclerViewListaContatos.setLayoutManager( layoutManager );
         recyclerViewListaContatos.setHasFixedSize( true );
         recyclerViewListaContatos.setAdapter( adapter );
+
+        //Configurar evento de clique no recyclerView
+        recyclerViewListaContatos.addOnItemTouchListener(
+                new RecyclerItemClickListener(
+                        getActivity(),
+                        recyclerViewListaContatos,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Intent intent = new Intent(getActivity(), ChatActivity.class);
+                                startActivity( intent );
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                            }
+                        }
+                )
+        );
 
         return view;
     }
@@ -88,9 +121,16 @@ public class ContatosFragment extends Fragment {
                 //Percorre todos os usuários no firebase
                 for (DataSnapshot dados : dataSnapshot.getChildren()) {
 
+                    FirebaseUser usuarioAtual = UsuarioFirebase.getUsuarioAtual();
+
                     //Pega um objeto do tipo usuário
                     Usuario usuario = dados.getValue( Usuario.class );
-                    listaContatos.add( usuario );
+
+                    //Não adiciona o usuário atual na lista de contatos
+                    String emailUsuarioAtual = usuarioAtual.getEmail();
+                    if( !emailUsuarioAtual.equals(usuario.getEmail()) ) {
+                        listaContatos.add( usuario );
+                    }
                 }
 
                 //Notificar que os dados foram modificados
