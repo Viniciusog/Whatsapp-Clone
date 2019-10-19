943de2ee15b024e20089edc49e88b8aa4944b7a1
Viniciusog <vinijipa23@gmail.com>
2019-10-19 01:24:11 -0400

    configure group activity

---
+++ app/src/main/java/com/viniciusog/whatsapp/activity/CadastroGrupoActivity.java
@@ -0,0 +1,74 @@
+package com.viniciusog.whatsapp.activity;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.View;
+import android.widget.TextView;
+
+import com.viniciusog.whatsapp.R;
+import com.viniciusog.whatsapp.adapter.GrupoSelecionadoAdapter;
+import com.viniciusog.whatsapp.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CadastroGrupoActivity extends AppCompatActivity {
+
+    private List<Usuario> listaMembrosSelecionados = new ArrayList<>();
+    private TextView textTotalParticipantes;
+    private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
+    private RecyclerView recyclerMembrosSelecionados;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_grupo);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Novo Grupo");
+        toolbar.setSubtitle("Insira um nome");
+        setSupportActionBar(toolbar);
+
+        //Configurações iniciais
+        textTotalParticipantes = findViewById(R.id.textTotalParticipantes);
+        recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosGrupo);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Recuperar lista de membros passadas
+        if (getIntent().getExtras() != null) {
+            List<Usuario> membros = (List<Usuario>) getIntent().getExtras().getSerializable("membros");
+            listaMembrosSelecionados.addAll( membros );
+
+            textTotalParticipantes.setText("Participantes: " + listaMembrosSelecionados.size());
+        }
+
+        //Configurar recycler view
+        grupoSelecionadoAdapter = new GrupoSelecionadoAdapter(listaMembrosSelecionados, getApplicationContext());
+
+        //Configurar layout manager
+        RecyclerView.LayoutManager layoutManagerHorizontal = new LinearLayoutManager(getApplicationContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false);
+
+        recyclerMembrosSelecionados.setLayoutManager(layoutManagerHorizontal);
+        recyclerMembrosSelecionados.setHasFixedSize(true);
+        recyclerMembrosSelecionados.setAdapter(grupoSelecionadoAdapter);
+    }
+
+}
--- app/src/main/java/com/viniciusog/whatsapp/activity/GrupoActivity.java
+++ app/src/main/java/com/viniciusog/whatsapp/activity/GrupoActivity.java
@@ -1,35 +1,206 @@
 package com.viniciusog.whatsapp.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.View;
+import android.widget.AdapterView;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.viniciusog.whatsapp.Helper.RecyclerItemClickListener;
+import com.viniciusog.whatsapp.Helper.UsuarioFirebase;
 import com.viniciusog.whatsapp.R;
+import com.viniciusog.whatsapp.adapter.ContatosAdapter;
+import com.viniciusog.whatsapp.adapter.GrupoSelecionadoAdapter;
+import com.viniciusog.whatsapp.config.ConfiguracaoFirebase;
+import com.viniciusog.whatsapp.model.Usuario;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GrupoActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerMembros, recyclerMembrosSelecionados;
+    private ContatosAdapter contatosAdapter;
+    private GrupoSelecionadoAdapter grupoSelecionadoAdapter;
+    private List<Usuario> listaMembros = new ArrayList<>();
+    private List<Usuario> listaMembrosSelecionados = new ArrayList<>();
+    private ValueEventListener valueEventListenerMembros;
+    private DatabaseReference usuariosRef;
+    private FirebaseUser usuarioAtual;
+    private Toolbar toolbar;
+    private FloatingActionButton fabAvancarCadastro;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grupo);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle("Novo Grupo");
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Configurações inciais
+        recyclerMembros = findViewById(R.id.recyclerMembros);
+        recyclerMembrosSelecionados = findViewById(R.id.recyclerMembrosSelecionados);
+        usuariosRef = ConfiguracaoFirebase.getFirebaseDatabase().child("usuarios");
+        usuarioAtual = UsuarioFirebase.getUsuarioAtual();
+        fabAvancarCadastro = findViewById(R.id.fab);
+
+        //Configurar adapter
+        contatosAdapter = new ContatosAdapter(listaMembros, getApplicationContext());
+
+        //Configurar layout manager
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerMembros.setLayoutManager(layoutManager);
+        recyclerMembros.setHasFixedSize(true);
+        recyclerMembros.setAdapter(contatosAdapter);
+
+        recyclerMembros.addOnItemTouchListener(new RecyclerItemClickListener(
+                getApplicationContext(),
+                recyclerMembros, new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Usuario usuarioSelecionado = listaMembros.get(position);
+
+
+                //Remover  usuário selecionado da dlista
+                listaMembros.remove(usuarioSelecionado);
+                contatosAdapter.notifyDataSetChanged();
+
+                listaMembrosSelecionados.add(usuarioSelecionado);
+                grupoSelecionadoAdapter.notifyDataSetChanged();
+                atualizarMembrosToolbar();
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        }
+        ));
+
+        //Configurar recycler view para os membros selecionados
+        grupoSelecionadoAdapter = new GrupoSelecionadoAdapter(listaMembrosSelecionados, getApplicationContext());
+
+        //Configurar layout manager
+        RecyclerView.LayoutManager layoutManagerHorizontal = new LinearLayoutManager(getApplicationContext(),
+                LinearLayoutManager.HORIZONTAL,
+                false);
+
+        recyclerMembrosSelecionados.setLayoutManager(layoutManagerHorizontal);
+        recyclerMembrosSelecionados.setHasFixedSize(true);
+        recyclerMembrosSelecionados.setAdapter(grupoSelecionadoAdapter);
+
+
+        recyclerMembrosSelecionados.addOnItemTouchListener(new RecyclerItemClickListener(
+                getApplicationContext(),
+                recyclerMembrosSelecionados,
+                new RecyclerItemClickListener.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Usuario usuarioSelecionado = listaMembrosSelecionados.get( position );
+
+                //remover da listagem de membros selecionados
+                listaMembrosSelecionados.remove( usuarioSelecionado );
+                grupoSelecionadoAdapter.notifyDataSetChanged();
+
+                //adicionar à listagem de membros
+                listaMembros.add( usuarioSelecionado );
+                contatosAdapter.notifyDataSetChanged();
+                atualizarMembrosToolbar();
+            }
+
+            @Override
+            public void onLongItemClick(View view, int position) {
+
+            }
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        }
+        ));
+
+        //Configurar clique em fab
+        fabAvancarCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(GrupoActivity.this, CadastroGrupoActivity.class);
+                intent.putExtra("membros", (Serializable) listaMembrosSelecionados);
+                startActivity( intent );
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarContatos();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        usuariosRef.removeEventListener(valueEventListenerMembros);
+    }
+
+    public void recuperarContatos() {
+
+        valueEventListenerMembros = usuariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                //Percorre todos os usuários no firebase
+                for (DataSnapshot dados : dataSnapshot.getChildren()) {
+
+                    FirebaseUser usuarioAtual = UsuarioFirebase.getUsuarioAtual();
+
+                    //Pega um objeto do tipo usuário
+                    Usuario usuario = dados.getValue(Usuario.class);
+
+                    //Não adiciona o usuário atual na lista de contatos
+                    String emailUsuarioAtual = usuarioAtual.getEmail();
+                    if (!emailUsuarioAtual.equals(usuario.getEmail())) {
+                        listaMembros.add(usuario);
+                    }
+                }
+
+                //Notificar que os dados foram modificados
+                contatosAdapter.notifyDataSetChanged();
+                atualizarMembrosToolbar();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void atualizarMembrosToolbar(){
+        int totalSelecionados = listaMembrosSelecionados.size();
+        int total = listaMembros.size() + totalSelecionados;
+        toolbar.setSubtitle(totalSelecionados + " de " + total + " selecionados");
+    }
 }
